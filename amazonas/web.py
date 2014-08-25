4b0abe2c005d4a759b9afbc3402cdcfcda16acfe
Atzm Watanabe <atzm@atzm.org>
2014-08-25 18:32:25 +0900

    web: change config section name appropriately

--- amazonas/web.py
+++ amazonas/web.py
@@ -20,14 +20,14 @@ def main():
     app = flask.Flask(__name__)
     app.config['JSON_AS_ASCII'] = False
 
-    for i in config.getlist('server', 'instances'):
+    for i in config.getlist('web', 'instances'):
         webapp.instance.register(i, textgen.TextGenerator.getinstance(i))
 
     try:
         app.register_blueprint(webapp.v01.mod)
-        app.run(host=config.get('server', 'host'),
-                port=config.getint('server', 'port'),
-                debug=config.getboolean('server', 'debug'),
+        app.run(host=config.get('web', 'host'),
+                port=config.getint('web', 'port'),
+                debug=config.getboolean('web', 'debug'),
                 use_reloader=False)
     finally:
         webapp.instance.unregister()
